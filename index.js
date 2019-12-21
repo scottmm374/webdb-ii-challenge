3916d8ee27e9d0b63a9d092c01582755bc351623
Michelle Scott <scottmm374@gmail.com>
2019-12-20 21:49:41 -0500

    Read works

--- index.js
+++ index.js
@@ -4,26 +4,12 @@ const carsRouter = require("./routers/carRouter");
 
 const port = 5000;
 server.use(express.json());
+server.use("/api/cars", carsRouter);
+
+server.get("/", (req, res) => {
+  res.json({ Message: "Welcome to Cars DB" });
+});
 
 server.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
-
-// exports.up = async function(knex) {
-//     await knex.schema.createTable("cars", table => {
-//       table.increments("Id");
-//       table
-//         .string("VIN", 17)
-//         .notNull()
-//         .unique();
-//       table.text("Make").notNull();
-//       table.text("Model").notNull();
-//       table.integer("Mileage").notNull();
-//       table.text("Transmission_Type");
-//       table.text("Title_Status");
-//     });
-//   };
-
-//   exports.down = async function(knex) {
-//     await knex.schema.dropTableIfExists("cars");
-//   };
--- routers/carRouter.js
+++ routers/carRouter.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const db = require("../utils/db");
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const cars = await db("cars").select();
+    res.json(cars);
+  } catch (err) {
+    console.log(err);
+  }
+});
 module.exports = router;

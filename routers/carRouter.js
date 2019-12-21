942facc0c0b3244fc00fa6c8fa23c173f835329b
Michelle Scott <scottmm374@gmail.com>
2019-12-21 15:53:30 -0500

    added edit and delete

--- routers/carRouter.js
+++ routers/carRouter.js
@@ -22,4 +22,39 @@ router.post("/", async (req, res) => {
     console.log(err);
   }
 });
+
+router.put("/:id", async (req, res) => {
+  try {
+    const payload = {
+      VIN: req.body.VIN,
+      Make: req.body.Make,
+      Model: req.body.Model,
+      Mileage: req.body.Mileage,
+      Transmission_Type: req.body.Transmission_Type || null,
+      Title_Status: req.body.Title_Status || null
+    };
+    await db("cars")
+      .where("id", req.params.id)
+      .update(payload);
+    res.json(
+      await db("cars")
+        .where("id", req.params.id)
+        .first()
+    );
+    // res.status(201).json(updateCar);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await db("cars")
+      .where("id", req.params.id)
+      .del();
+    res.status(200).end();
+  } catch (err) {
+    console.log(err);
+  }
+});
 module.exports = router;
